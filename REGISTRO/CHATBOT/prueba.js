c6a2b371e6aac008e77a34cc27959d036eaa10e2
QingYu <qingyu.jiang@alumni.mondragon.edu>
2025-03-27 10:07:31 +0100

    UPD: pruebas añadir js local en chatbot

--- REGISTRO/CHATBOT/prueba.js
+++ REGISTRO/CHATBOT/prueba.js
@@ -1,3 +1,21 @@
-window.onload = function() {
+window.onload = function () {
     console.log("Todos los recursos de la página han sido cargados.");
+
+    document.getElementById("bubbleChat").onclick = function () {
+        console.log("Frame click");
+    };
+
+    // Observador de cambios en el DOM
+    const observer = new MutationObserver((mutations, obs) => {
+        let bubble = document.getElementById("chatbase-bubble-button");
+        if (bubble) {
+            console.log("Elemento encontrado, asignando evento.");
+            bubble.onclick = function () {
+                console.log("bubble click");
+            };
+            obs.disconnect(); // Dejar de observar una vez encontrado
+        }
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
 };
